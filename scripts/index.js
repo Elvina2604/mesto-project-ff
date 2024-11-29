fe12b9a228bef925a5895ca1b39263a15208c6b6
Elvina Zaynullina <Elvina2604@yandex.ru>
2024-11-29 23:12:51 +0300

    second commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
+function createCard(cardAttribute, deleteAttribute) {
+    const template = document.querySelector('#card-template');
+    const item = template.content.cloneNode(true);
+    const templateCard = item.querySelector('.card');
+    const templateImage = item.querySelector('.card__image');
+    const templateTitle = item.querySelector('.card__title');
+    templateImage.src = cardAttribute.link;
+    templateImage.alt = cardAttribute.alt;
+    templateTitle.textContent = cardAttribute.name;
 
+    const deleteButton = templateCard.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteAttribute);
+    return templateCard;
+}
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
 
 // @todo: Функция удаления карточки
-
+function deleteCard(buttonEvent) {
+    const listItem = buttonEvent.target.closest('.card');
+    listItem.remove();
+}
 // @todo: Вывести карточки на страницу
+const displayCard = document.querySelector('.places__list');
+initialCards.map((placesList) => {
+    const templateCard = createCard(placesList, deleteCard);
+    displayCard.append(templateCard);
+});
